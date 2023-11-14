453a84985bdf5b98b6fa16366636390e01589c01
David <70871401+lordaval@users.noreply.github.com>
2023-11-14 16:26:54 -0300

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -12,13 +12,12 @@ import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage'
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyB80aLYVmQeQkaIqc9OkpF9XF6ux7i9WKo",
-  authDomain: "arcanjos-app.firebaseapp.com",
-  projectId: "arcanjos-app",
-  storageBucket: "arcanjos-app.appspot.com",
-  messagingSenderId: "817259790782",
-  appId: "1:817259790782:web:4b342c892f6bf90340b9ef",
-  measurementId: "G-34TV7CQR64"
+  apiKey: "AIzaSyCwIO9tEedU2fE9hF9JILL4ov8z_tHc_4I",
+  authDomain: "arcanjos-app-5a63f.firebaseapp.com",
+  projectId: "arcanjos-app-5a63f",
+  storageBucket: "arcanjos-app-5a63f.appspot.com",
+  messagingSenderId: "828111836196",
+  appId: "1:828111836196:web:e6201147ce7153296234b1"
 };
 
 const app = initializeApp(firebaseConfig);
