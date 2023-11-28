765513e2803e79e10b141f37744773cd9e09b884
lordaval <lordaval1117@gmail.com>
2023-11-28 20:01:58 -0300

    .

--- src/screens/Estoque/index.tsx
+++ src/screens/Estoque/index.tsx
@@ -2,31 +2,30 @@ import { View, SafeAreaView, StyleSheet, Text } from 'react-native'
 import React from 'react'
 
 export default function Estoque() {
-    return (
-        <SafeAreaView style={styles.screen}>
+  return (
+    <SafeAreaView style={styles.screen}>
       <View style={styles.header}>
-        <Text style={styles.title}>EWstoque!</Text>
+        <Text style={styles.title}>Estoque</Text>
       </View>
     </SafeAreaView>
-    )
+  )
 }
 
 const styles = StyleSheet.create({
-    screen: {
-      flex: 1,
-      paddingTop: 20,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-    },
-    header: {
-      width: '100%',
-      padding: 16,
-      alignItems: 'flex-start',
-    },
-    title: {
-      fontSize: 24,
-      fontWeight: 'bold',
-      textAlign: 'center',
-    },
-    
-  })
+  screen: {
+    flex: 1,
+    paddingTop: 20,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+  },
+  header: {
+    width: '100%',
+    padding: 16,
+    alignItems: 'flex-start',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+})
